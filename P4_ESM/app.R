cbea7ab644eb95c6b47d7746c9a38a98f467bef0
CaileyFay <fayc25@wfu.edu>
2026-02-20 23:01:11 -0500

    added inputs to the dashboard, working on getting the figure to adjust with the inputs, and then it will be done

--- P4_ESM/app.R
+++ P4_ESM/app.R
@@ -20,27 +20,39 @@ ui <- fluidPage(
 
     sidebarLayout(
       sidebarPanel(
-      sliderInput("year", "Year", min = 1999, max = 2011,
-                  value = c(2005,2010),
-      radioButtons("value", "Value",
-                   choices = c("deaths","cases","mortalityRate", "deathsPerMillion"),
-                   selected = "deaths")),
-      mainPanel(plotOutput("coolplot"),
+      sliderInput("hour", "Time of Day", min = 0, max = 24, #my slide is going to allow me to isolate times of day
+                  value = c(9,18)),
+
+       radioButtons("who_are_you", "Subject",
+                   choices = c("Cailey","Ethan"), #I can select a button to show me the graph for Cailey or Ethan
+                   selected = "Cailey")),
+
+        selectInput("selected_trait", "Choose Trait", #I needed help from Gemini with this because I didn't know if I could select multiple columns to choose from
+                 choices = c("Extraversion","Agreeableness","Conscientiousness","Emotional_Stability","Openness"),
+                selected = "Extraversion")), #it appears to be working!
+
+         mainPanel(plotOutput("coolplot"),
                 tableOutput("cooltable"))
-      )))
+      )
 
 # Define server logic
 server <- function(input, output) {
 
   output$coolplot <- renderPlot({
 
-    bsl %>%
-      filter(Year >= input$year[1],
-             Year <= input$year[2],
-             stat == input$stat) %>%
-    ggplot(aes(x=year, y=value)) +
-      geom_point() +
-      geom_smooth(method = "lm")
+      syn_data %>%
+     filter(hour >= input$hour[1],
+           hour <= input$hour[2]) %>%
+       #      who_are_you == input$who_are_you,  ------------------this part of the code breaks the figure
+        #    selected_trait == input$selected_trait) %>% ------------and this part
+
+         ggplot(mapping = aes(x=hour, y=Extraversion, fill = who_are_you)) +
+      geom_col() +
+      labs(title = "Extraversion at Different Times of Day, For Different Subjects",
+           x = "Time of Day, in Military Time",
+           y = "Extraversion Level",
+           fill = "Subject") +
+      theme_classic()
   })
 }
 
